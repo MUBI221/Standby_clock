cb5d3085f8c569f79928a05fe846d9a0ab3c1e20
SHEIKH MUBASHIR <89804227+MUBI221@users.noreply.github.com>
2023-11-21 23:24:07 +0500

    Initial commit

---
+++ app/src/main/java/com/example/standby_clock/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.standby_clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextClock;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextClock textClock,textampm,textdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+        getWindow ().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow ().addFlags (WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        textClock = findViewById (R.id.textclockid);
+        textampm = findViewById (R.id.textampmid);
+        textdate =findViewById (R.id.textdateid);
+
+
+    }
+}
